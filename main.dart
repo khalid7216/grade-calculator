722ebe504b3d6b4703f261ed2b26a3fd1efc9d9e
khalid7216 <156210757+khalid7216@users.noreply.github.com>
2024-01-18 17:58:17 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+void main (){
+String stname = "khalid";
+String stID = "K00155";
+int Totalnumber = 500;
+int ObtNumber = 100;
+num percentage = ObtNumber /Totalnumber *100;
+stdout.write('Obtain marks');
+double ObtNumber = double.parse(stdin.readLineSync()!);
+if( percentage >= 90){
+  print('print ur grade is A1 and percentage $percentage%');
+}
+else if(percentage >= 80 ){
+    print('print ur grade is A1 and percentage $percentage%');
+  
+} 
+
+}
